3c631cf9c51318c699afca0930e74946428b1133
arfin97 <arfin97@gmail.com>
2024-01-13 23:47:21 +0600

    fixed userclass error

--- zoom/BasicUser.java
+++ zoom/BasicUser.java
@@ -1,20 +1,20 @@
-package zoom;
-
-public class BasicUser extends User {
-
-    private int monthlyBasicCharge;
-
-    public BasicUser(String name, int age, String email, String password, String address, String phone, int monthlyBasicCharge) {
-        super(name, age, email, password, address, phone);
-        this.monthlyBasicCharge = monthlyBasicCharge;
-    }
-
-    public int getMonthlyBasicCharge() {
-        return monthlyBasicCharge;
-    }
-
-    @Override
-    public int monthlyCharge() {
-        return 30 * monthlyBasicCharge;
-    }
-}
+//package zoom;
+//
+//public class BasicUser extends User {
+//
+//    private int monthlyBasicCharge;
+//
+//    public BasicUser(String name, int age, String email, String password, String address, String phone, int monthlyBasicCharge) {
+//        super(name, age, email, password, address, phone);
+//        this.monthlyBasicCharge = monthlyBasicCharge;
+//    }
+//
+//    public int getMonthlyBasicCharge() {
+//        return monthlyBasicCharge;
+//    }
+//
+//    @Override
+//    public int monthlyCharge() {
+//        return 30 * monthlyBasicCharge;
+//    }
+//}
--- zoom/HostUser.java
+++ zoom/HostUser.java
@@ -1,31 +1,31 @@
-package zoom;
-
-public class HostUser extends User {
-    private String hostId;
-    private int monthlyHostCharge;
-
-    public HostUser(String name, int age, String email, String password, String address, String phone, String hostId, int monthlyCharge) {
-        super(name, age, email, password, address, phone);
-        this.hostId = hostId;
-        this.monthlyHostCharge = monthlyCharge;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + ", HostId: " + hostId + ", MonthlyCharge: " + monthlyHostCharge;
-    }
-
-    public int getMonthlyHostCharge() {
-        return monthlyHostCharge;
-    }
-
-    @Override
-    public int monthlyCharge() {
-        return 30 * monthlyHostCharge;
-    }
-
-    public void printParent(){
-        System.out.println(super.toString());
-    }
-
-}
+//package zoom;
+//
+//public class HostUser extends User {
+//    private String hostId;
+//    private int monthlyHostCharge;
+//
+//    public HostUser(String name, int age, String email, String password, String address, String phone, String hostId, int monthlyCharge) {
+//        super(name, age, email, password, address, phone);
+//        this.hostId = hostId;
+//        this.monthlyHostCharge = monthlyCharge;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return super.toString() + ", HostId: " + hostId + ", MonthlyCharge: " + monthlyHostCharge;
+//    }
+//
+//    public int getMonthlyHostCharge() {
+//        return monthlyHostCharge;
+//    }
+//
+//    @Override
+//    public int monthlyCharge() {
+//        return 30 * monthlyHostCharge;
+//    }
+//
+//    public void printParent(){
+//        System.out.println(super.toString());
+//    }
+//
+//}
