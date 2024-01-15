9014f625e88332ed7419bdaa61aa657687ae93f4
arfin97 <arfin97@gmail.com>
2024-01-15 23:48:20 +0600

    overloading example

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        UserService userService = new UserService(new OracleConnection());
-        userService.saveUser("John Doe");
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(1, 2));
     }
 }
