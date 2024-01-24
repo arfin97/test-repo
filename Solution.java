9336f9fa26fd7d44083c824d0bc4feda61433ded
arfin97 <arfin97@gmail.com>
2024-01-24 23:56:07 +0600

    updated code

--- Solution.java
+++ Solution.java
@@ -3,9 +3,9 @@ class Solution {
     public static void main(String[] args) {
         System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
     }
-    public static boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "");
-        String reversed = new StringBuilder(s.toLowerCase()).reverse().toString();
-        return s.toLowerCase().equals(reversed);
+    public static boolean isPalindrome(String sy) {
+        sy = sy.replaceAll("[^a-zA-Z0-9]", "");
+        String reversed = new StringBuilder(sy.toLowerCase()).reverse().toString();
+        return sy.toLowerCase().equals(reversed);
     }
 }
